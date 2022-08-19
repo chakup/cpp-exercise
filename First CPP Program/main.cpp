3360517330d48d20d71d9d6194c5be3920485df0
Jakup Karagoz <jakar19@student.sdu.dk>
2022-08-19 22:41:00 +0200

    statements and functions added

---
+++ First CPP Program/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //print things in console
+
+int main()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Jakup" << std::endl;
+    }
+
+    /*    std::cout << "Hello World!" << std::endl; */ // std::endl -- means endline and makes a new line
+    return 0;
+}
