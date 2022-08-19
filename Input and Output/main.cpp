70052ed4cafb883af0e5d85084a7f766c92ea6d2
Jakup Karagoz <jakar19@student.sdu.dk>
2022-08-19 23:28:51 +0200

    Added io exercise

--- Input and Output/main.cpp
+++ Input and Output/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    // Printing data
+
+    std::cout << "Hello C++20 " << std::endl;
+
+    int age{21};
+    std::cout << "Age : " << age << std::endl;
+
+    std::cerr << "Error message : Something is wrong " << std::endl;
+    std::clog << "Log message : Something happened" << std::endl;
+
+    // Input
+    /*int age1;
+    std::string name;
+
+    std::cout << "Please type your name : " << std::endl;
+
+    std::cin >> name;
+
+    std::cout << "Please type your age : " << std::endl;
+    std::cin >> age1;
+
+    std::cout << "Hello " << name << " you are " << age1 << " years old " << std::endl;
+
+    std::cout << "Please type your name and age : " << std::endl;
+
+    std::cin >> name >> age1;
+
+    std::cout << "Hello " << name << " you are " << age1 << " years old " << std::endl;*/
+
+    // Data with spaces (eg first and lastname)
+    std::string full_name;
+    int age3;
+
+    std::cout << "Please type in your full name and age" << std::endl;
+
+    std::getline(std::cin, full_name);
+
+    std::cin >> age3;
+
+    std::cout << "Hello " << full_name << " you are " << age3 << " years old " << std::endl;
+
+    return 0;
 }
