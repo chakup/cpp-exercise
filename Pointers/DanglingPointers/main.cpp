00d8a132c6fb8bceed5a2615716894390fc2adec
Jakup Karagoz <jakar19@student.sdu.dk>
2022-08-23 19:02:15 +0200

    dangling pointers added

---
+++ Pointers/DanglingPointers/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+    // Case1 : Uninitialized pointer
+    int *p_number; // Dangling unintialized pointer
+
+    std::cout << std::endl;
+    std::cout << "Case 1 : Uninitialied pointer : ." << std::endl;
+    std::cout << "p_number : " << p_number << std::endl;
+    // std::cout << "*p_number : " << *p_number << std::endl; //CRASH!
+
+    // Case 2 : deleted pointer
+    std::cout << std::endl;
+    std::cout << "Case 2 : Deleted pointer " << std::endl;
+    int *p_number1{new int{67}};
+
+    std::cout << "*p_number1 (before delete : " << *p_number1 << std::endl;
+    delete p_number1;
+    std::cout << "*p_number1(after delete) : " << *p_number1 << std::endl;
+
+    // Case 3: multiple pointers pointing to same address
+    std::cout << std::endl;
+
+    std::cout << "Case 3 : Multiple pointers pointing to same address : " << std::endl;
+
+    int *p_number3{new int{83}};
+    int *p_number4{p_number3};
+
+    std::cout << "p_number3 - " << p_number3 << " - " << *p_number3 << std::endl;
+    std::cout << "p_number4 - " << p_number4 << " - " << *p_number4 << std::endl;
+
+    delete p_number3;
+
+    // p_number4 points to deleted memory. Dereferencing it will lead to undefined behaviour : Crash/garbage or whatever
+    std::cout << "p_number4(after deleting p_number3) - " << p_number4 << " - " << *p_number4 << std::endl;
+    return 0;
+}
