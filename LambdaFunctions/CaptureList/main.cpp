a3b6f4222c5d83ef35fafec2a8ade350d9668cfa
Jakup Karagoz <jakar19@student.sdu.dk>
2022-08-30 19:37:06 +0200

    new

---
+++ LambdaFunctions/CaptureList/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+
+    // Capture lists
+    double a{10};
+    double b{20};
+
+    auto func = [a, b]()
+    {
+        std::cout << "a + b : " << a + b << std::endl;
+    };
+    func();
+
+    double c{42};
+
+    auto func = [c]() { // C is just a copy and not the actual variable
+        std::cout << "Inner value : " << c << "address of inner" << &c << std::endl;
+    };
+
+    for (size_t i{}; i < 5; ++i)
+    {
+        std::cout << "Outer value : " << c << "address of outer" << &c << std::endl;
+        func();
+        ++c;
+    }
+
+    return 0;
+}
