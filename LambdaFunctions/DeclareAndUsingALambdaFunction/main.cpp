31a79a9cc59e8f7714500be69fbc20f668bebc85
Jakup Karagoz <jakar19@student.sdu.dk>
2022-08-30 16:18:59 +0200

    exercise added

---
+++ LambdaFunctions/DeclareAndUsingALambdaFunction/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+
+    /*
+        Lambda function signature:
+                                    [capture list] (parameters) -> return type {
+                                        //Function body
+                                    }
+*/
+
+    // Declaring a lambda function and calling it through a name
+    /*auto func = []()
+    {
+        std::cout << "Hello world!" << std::endl;
+    };
+    func();
+    */
+    /*
+  auto result = [](double a, double b)
+  {
+      return (a + b);
+  }(12.1, 5.7);*/
+
+    // Declare a lambda function and call it directly
+    []()
+    {
+        std::cout << "Hello world!" << std::endl;
+    }();
+
+    // Lambda function that takes parameters
+    auto func1 = [](double a, double b)
+    {
+        std::cout << "a+b : " << (a + b) << std::endl;
+    };
+    func1(10.2, 2.3);
+
+    return 0;
+}
